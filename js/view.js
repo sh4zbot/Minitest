f9bd325e74ae2146e49d47221aec1af49fa03a03
Johan Lindeberg <jlindebe@kth.se>
2015-03-29 16:35:35 +0200

    Progress bar, points, info and exit buttons updated

--- js/view.js
+++ js/view.js
@@ -1,6 +1,8 @@
 //DishdetailView Object constructor
 var View = function (model) {
-
+	
+	model.addObserver(this);
+	
 	this.questionNumber = document.getElementById("questionNumber");
 	this.question = document.getElementById("question");
 	this.sound = document.getElementById("sound");
@@ -8,40 +10,45 @@ var View = function (model) {
 	this.plusscore = document.getElementById("plusscore");
 	this.negscore = document.getElementById("negscore");
 	
-	/*
-	this.ansButtons = [];
-	for (i=1; i<=4; i++) {
-		this.ansButtons.push(document.getElementById("answer" + i));
-	}
-	console.log("this.ansButtons: " + this.ansButtons);
-	*/
 	
+	//BUTTONS
 	this.ans1 = document.getElementById("answer1");
 	this.ans2 = document.getElementById("answer2");
 	this.ans3 = document.getElementById("answer3");
 	this.ans4 = document.getElementById("answer4");
+	this.ansbtns = new Array();
+	this.ansbtns.push(this.ans1);
+	this.ansbtns.push(this.ans2);
+	this.ansbtns.push(this.ans3);
+	this.ansbtns.push(this.ans4);
 	
 	this.listenBtn = document.getElementById("listen");
 	
+	//STATIC SOUNDS
 	this.correctSnd = document.getElementById("correctSnd");
 	this.wrongSnd = document.getElementById("wrongSnd");
 	this.scoreSnd = document.getElementById("scoreSnd");
-	
-	model.addObserver(this);
+
 	
 	this.update = function () {
 		this.questionNumber.innerHTML = model.getState() + 1;
 		this.question.innerHTML = model.getQuestion();
 		var currAnswers = model.getAnswersTuple();
-		
-		this.ans1.innerHTML = "<span>" + currAnswers[0].answer + "</span>";
-		this.ans2.innerHTML = "<span>" + currAnswers[1].answer + "</span>";
-		this.ans3.innerHTML = "<span>" + currAnswers[2].answer + "</span>";
-		this.ans4.innerHTML = "<span>" + currAnswers[3].answer + "</span>";
-		
-		this.score.innerHTML = "puntos: " + model.getScore() + "/" + model.getLength();
-		this.plusscore.innerHTML = "" + model.getPlusScore();
-		this.negscore.innerHTML = "" + model.getNegScore();
+	
+		for (i = 0; i<4;i++) {
+			
+			if (currAnswers[i].answer.length > 30) {
+				this.ansbtns[i].innerHTML = "<span>" + currAnswers[i].answer.slice(0,30) + "<br/>" +  
+				currAnswers[i].answer.slice(30,(currAnswers[i].answer.length -1)) + "</span>";
+			}
+			else {
+				this.ansbtns[i].innerHTML = "<span>" + currAnswers[i].answer + "</span>";
+			}
+		}
+			
+		this.score.innerHTML = "puntos " + model.getScore() + "/" + model.getLength() * 4;
+		this.plusscore.innerHTML = "+" + model.getPlusScore();
+		this.negscore.innerHTML = "-" + model.getNegScore();
 		
 		
 		
@@ -59,7 +66,11 @@ var View = function (model) {
 			this.listenBtn.style.display = 'none';
 			this.sound.innerHTML = "";
 		}
-		
+		var plusStyleWidth = ( model.getPlusScore() / (model.getLength() * 4) ) * 100;
+		var negStyleWidth = ( model.getNegScore() / (model.getLength()*4 ) * 100);
+		console.log("neg style width: " + negStyleWidth);
+		document.getElementById("posprog").style.width = plusStyleWidth  + "%";
+		document.getElementById("negprog").style.width = negStyleWidth + "%";
 	}
 	
 	this.btnReset = function (ansBtns) {
